59d368745bdb91c99352b46bd572a6d79f6a215a
lvl0nax <lvl0nax@gmail.com>
2024-01-31 13:27:32 +0000

    finish batcher

--- batcher/main.go
+++ batcher/main.go
@@ -2,6 +2,7 @@ package batcher
 
 import (
 	"sync"
+	"time"
 )
 
 type Batcher struct {
@@ -25,22 +26,59 @@ func NewBatcher(size int, action func([]string)) *Batcher {
 	}
 }
 
-func (b *Batcher) Append() {
+func (b *Batcher) Append(message string) {
+	select {
+	case <-b.closeCh:
+		return
+	default:
+	}
 
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	b.messages = append(b.messages, message)
+	if len(b.messages) >= b.size {
+		b.batchesCh <- b.messages
+		b.messages = []string{}
+	}
 }
 
-func (b *Batcher) Run() {
-	go func() {
-		defer close(b.closeDoneCh)
-		for {
-			select {
-			case <-b.closeCh:
-				return
-			case batch := <-b.batchesCh:
-				b.action(batch)
-			}
-		}
+func (b *Batcher) Run(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer func() {
+		ticker.Stop()
+		close(b.batchesCh)
 	}()
+
+	for {
+		select {
+		case <-b.closeCh:
+			b.flush()
+		default:
+		}
+
+		select {
+		case <-b.closeCh:
+			b.flush()
+			return
+		case messages := <-b.batchesCh:
+			b.action(messages)
+			ticker.Reset(interval)
+		case <-ticker.C:
+			b.flush()
+		}
+	}
+}
+
+func (b *Batcher) flush() {
+	b.mutex.Lock()
+	messages := b.messages
+	b.messages = []string{}
+	b.mutex.Unlock()
+
+	if len(messages) > 0 {
+		b.action(messages)
+	}
 }
 
 func (b *Batcher) Shutdown() {
